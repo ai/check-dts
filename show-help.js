fea62643cd42a8b841d3420fd31f54e4308c7fb2
Andrey Sitnik <andrey@sitnik.ru>
2020-02-09 13:34:18 -0500

    Show tool description in --help

--- show-help.js
+++ show-help.js
@@ -10,6 +10,7 @@ let b = chalk.bold
 module.exports = function showHelp () {
   print(
     b('Usage: ') + 'npx check-dts',
+    'Check `.d.ts` files in open source library according to types tests',
     '',
     b('Arguments:'),
     '  ' + y('--version') + '   Show version',
