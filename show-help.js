06bf5ebd78883211cc92d93bb191c2dbc69d938a
Michal Czapracki <cz@signicode.com>
2020-10-27 22:35:15 +0000

    Add passing of specific files to check

--- show-help.js
+++ show-help.js
@@ -2,11 +2,14 @@ let { yellow: y, bold: b } = require('colorette')
 
 module.exports = function showHelp (print) {
   print(
-    b('Usage: ') + 'npx check-dts',
+    b('Usage: ') + 'npx check-dts [file1 file2...]',
     'Check `.d.ts` files in open source library according to types tests',
     '',
     b('Arguments:'),
+    '  ' +
+      y('file#') +
+      '       Optional a list of files/globs (otherwise matches all js/ts files)',
     '  ' + y('--version') + '   Show version',
-    '  ' + y('--help') + '   Show this message'
+    '  ' + y('--help') + '      Show this message'
   )
 }
