211d5e184d920822d6808036a3bd7de274f91382
Andrey Sitnik <andrey@sitnik.ru>
2021-11-17 23:20:14 +0100

    Fix TS 4.5 support

--- worker.js
+++ worker.js
@@ -4,4 +4,15 @@ import { createProgram } from './create-program.js'
 
 let { files, compilerOptions } = workerData
 
-parentPort.postMessage(createProgram(files, compilerOptions))
+let errors = createProgram(files, compilerOptions)
+let cleaned = errors.map(i => ({
+  messageText: i.messageText,
+  fileName: i.fileName,
+  start: i.start,
+  code: i.code,
+  file: {
+    fileName: i.file.fileName,
+    text: i.file.text
+  }
+}))
+parentPort.postMessage(cleaned)
