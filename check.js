d4691f07b2b1b8b22bf163e54458ee3863eaec44
Andrey Sitnik <andrey@sitnik.ru>
2024-08-20 23:01:09 +0000

    Improve CLI output on error

--- check.js
+++ check.js
@@ -75,113 +75,124 @@ export async function check(
   let spinner = createSpinner('Check types', { stream: stderr })
   spinner.start()
 
-  let compilerOptions = getCompilerOptions(cwd)
+  try {
+    let compilerOptions = getCompilerOptions(cwd)
 
-  let all = await glob(globs, { absolute: true, cwd, ignore: ['node_modules'] })
-
-  if (!all.some(i => /\.tsx?$/.test(i))) {
-    let err = new Error(
-      'TypeScript files were not found. ' +
-        'Create .d.ts files and test/types.ts and test/errors.ts.'
-    )
-    err.own = true
-    throw err
-  }
-
-  let typeTests = all.filter(i => /\.?(errors|types)\.tsx?/.test(i))
-  let failTests = all.filter(i => /\.?errors\.tsx?$/.test(i))
-
-  all.push(join(TS_DIR, 'lib.dom.d.ts'))
-  all.push(join(TS_DIR, 'lib.es2018.d.ts'))
-
-  let expects = await parseTest(failTests)
-  let errors = await checkFiles(all, compilerOptions)
-
-  let bad = {}
-  function push(file, message) {
-    if (!bad[file]) bad[file] = []
-    bad[file].push(message)
-  }
-
-  for (let error of errors) {
-    if (error.messageText.code === 6504 || error.messageText.code === 6054) {
-      // Unsupported files
-      continue
-    }
-
-    // istanbul ignore next
-    if (!error.file) {
-      // To support snapshot tests, remove absolute paths that would vary between machines.
-      let message = error.messageText.messageText.replaceAll(process.cwd(), '.')
-      push(error, message);
-      continue
-    }
-
-    let { column, line } = location(error.file.text).toPoint(error.start)
-    let expect = expects.find(j => {
-      return error.file.fileName === j.fileName && line === j.line && !j.used
+    let all = await glob(globs, {
+      absolute: true,
+      cwd,
+      ignore: ['node_modules']
     })
-    let prefix = r(
-      `✖ ${formatName(cwd, error.file.fileName, r)}:${line}:${column}:`
-    )
-    let text = getText(error)
-    if (!failTests.includes(error.file.fileName)) {
-      push(
-        error.fileName,
-        prefix +
-          b(' Type error ' + pico.gray(`TS${error.code}`) + '\n') +
-          '  ' +
-          r(text)
+
+    if (!all.some(i => /\.tsx?$/.test(i))) {
+      let err = new Error(
+        'TypeScript files were not found. ' +
+          'Create .d.ts files and test/types.ts and test/errors.ts.'
       )
-    } else if (!expect) {
-      push(error.fileName, prefix + b(' Unexpected error\n') + '  ' + r(text))
-    } else {
-      expect.used = true
-      if (!text.includes(expect.pattern)) {
+      err.own = true
+      throw err
+    }
+
+    let typeTests = all.filter(i => /\.?(errors|types)\.tsx?/.test(i))
+    let failTests = all.filter(i => /\.?errors\.tsx?$/.test(i))
+
+    all.push(join(TS_DIR, 'lib.dom.d.ts'))
+    all.push(join(TS_DIR, 'lib.es2018.d.ts'))
+
+    let expects = await parseTest(failTests)
+    let errors = await checkFiles(all, compilerOptions)
+
+    let bad = {}
+    function push(file, message) {
+      if (!bad[file]) bad[file] = []
+      bad[file].push(message)
+    }
+
+    for (let error of errors) {
+      if (error.messageText.code === 6504 || error.messageText.code === 6054) {
+        // Unsupported files
+        continue
+      }
+
+      // istanbul ignore next
+      if (!error.file) {
+        let message = error.messageText.messageText.replaceAll(
+          process.cwd(),
+          '.'
+        )
+        push(error, message)
+        continue
+      }
+
+      let { column, line } = location(error.file.text).toPoint(error.start)
+      let expect = expects.find(j => {
+        return error.file.fileName === j.fileName && line === j.line && !j.used
+      })
+      let prefix = r(
+        `✖ ${formatName(cwd, error.file.fileName, r)}:${line}:${column}:`
+      )
+      let text = getText(error)
+      if (!failTests.includes(error.file.fileName)) {
         push(
           error.fileName,
           prefix +
-            b(' Wrong error\n') +
-            '  Expected: ' +
-            g(expect.pattern) +
-            '\n' +
-            '  Got: ' +
+            b(' Type error ' + pico.gray(`TS${error.code}`) + '\n') +
+            '  ' +
             r(text)
         )
+      } else if (!expect) {
+        push(error.fileName, prefix + b(' Unexpected error\n') + '  ' + r(text))
+      } else {
+        expect.used = true
+        if (!text.includes(expect.pattern)) {
+          push(
+            error.fileName,
+            prefix +
+              b(' Wrong error\n') +
+              '  Expected: ' +
+              g(expect.pattern) +
+              '\n' +
+              '  Got: ' +
+              r(text)
+          )
+        }
       }
     }
-  }
-  let unused = expects.filter(i => !i.used)
-  for (let i of unused) {
-    push(
-      i.fileName,
-      r(`✖ ${formatName(cwd, i.fileName, r)}:${i.line}:${i.column}:`) +
-        b(' Error was not found\n') +
-        '  ' +
-        r(i.pattern)
-    )
-  }
+    let unused = expects.filter(i => !i.used)
+    for (let i of unused) {
+      push(
+        i.fileName,
+        r(`✖ ${formatName(cwd, i.fileName, r)}:${i.line}:${i.column}:`) +
+          b(' Error was not found\n') +
+          '  ' +
+          r(i.pattern)
+      )
+    }
 
-  let failed = Object.keys(bad).length > 0
-  if (failed) {
-    spinner.error()
-    print('')
-    for (let file in bad) {
-      let messages = (bad[file] || []).sort((msg1, msg2) => {
-        let line1 = parseInt(msg1.match(/(\d+):/)[1])
-        let line2 = parseInt(msg2.match(/(\d+):/)[1])
-        return line1 - line2
-      })
-      for (let i of messages) {
-        print(i + '\n')
+    let failed = Object.keys(bad).length > 0
+    if (failed) {
+      spinner.error()
+      print('')
+      for (let file in bad) {
+        let messages = (bad[file] || []).sort((msg1, msg2) => {
+          let line1 = parseInt(msg1.match(/(\d+):/)[1])
+          let line2 = parseInt(msg2.match(/(\d+):/)[1])
+          return line1 - line2
+        })
+        for (let i of messages) {
+          print(i + '\n')
+        }
       }
+      return false
+    } else {
+      spinner.success()
+      for (let i of typeTests) {
+        print(g('✔ ') + formatName(cwd, i, pico.white))
+      }
+      return true
     }
-    return false
-  } else {
-    spinner.success()
-    for (let i of typeTests) {
-      print(g('✔ ') + formatName(cwd, i, pico.white))
-    }
-    return true
+  } catch (e) {
+    spinner.error()
+    throw e
   }
 }
